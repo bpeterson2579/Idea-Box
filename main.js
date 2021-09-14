e2227f8652625638e53ae30a59e57d7412b8453d
Katie Ammon <katie.ammon10@gmail.com>
2021-09-14 16:30:26 -0400

    Add some querySelectors and refactored index.html

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 var ideas = [];
+
+var inputBoxes = document.querySelectorAll('.form-input');
+var saveButton = document.querySelector('.save-button');
+var searchIdeasButton = document.querySelector('.search-button');
+var searchIdeasInput = document.querySelector('.search-input');
+var gridContainer = document.querySelector('.grid-container');

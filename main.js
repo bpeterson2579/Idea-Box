0743e9744632e443a9e7b63e3f3895652044bf75
Katie Ammon <katie.ammon10@gmail.com>
2021-09-14 21:38:23 -0400

    Add displayIdeaCard funcion and innerHTML

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 var ideas = [];
+var ideaBox;
 
-var inputBoxes = document.querySelectorAll('.form-input');
+var inputTitle = document.querySelector('#titleInput');
+var inputBody = document.querySelector('#bodyInput');
+
 var saveButton = document.querySelector('.save-button');
 var searchIdeasButton = document.querySelector('.search-button');
 var searchIdeasInput = document.querySelector('.search-input');
 var gridContainer = document.querySelector('.grid-container');
+
+
+saveButton.addEventListener('click', createIdeaCard);
+
+function createIdeaCard() {
+  event.preventDefault();
+
+  ideaBox = new Idea(inputTitle.value, inputBody.value);
+  ideaBox.saveToStorage(ideaBox);
+
+  displayIdeaCard()
+}
+
+function displayIdeaCard() {
+  gridContainer.innerHTML = '';
+  for (var i = 0; i < ideas.length; i++) {
+    gridContainer.innerHTML += `
+      <div class="box">
+        <header class="card-header">
+          <img src="assets/star.svg" class="comment-star-delete-img">
+          <img src="assets/star-active.svg" class="card-star-active hidden">
+          <img src="assets/delete.svg" class="comment-star-delete-img">
+        </header>
+        <div class="user-idea">
+          <h4 class="user-title">${ideas[i].title}</h4>
+          <p>${ideas[i].body}</p>
+        </div>
+        <footer class="card-footer">
+          <img src="assets/comment.svg" class="comment-star-delete-img">
+          <p class="card-comment">Comment</p>
+        </footer>
+      </div>`
+  }
+}
